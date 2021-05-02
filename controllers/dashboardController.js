2d9755460e4663b6f298ac5d3a5142e25885077a
niazrahman <b170403065@sw.jnu.ac.bd>
2021-05-02 22:35:40 +0600

    conditionally used dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,6 +1,30 @@
 const Flash = require('../utils/Flash');
+const Profile = require('../models/Profile');
 
+exports.dashboardGetController = async (req,res,next) =>{
+    try{
+        let profile = await Profile.findOne({user : req.user._id})
+        if(profile){
+            return res.render('pages/dashboard/dashboard', {title : "My Dashboard",flashMessage: Flash.getMessage(req)});
+        }
 
-exports.dashboardGetController = (req,res,next) =>{
-    res.render('pages/dashboard/dashboard', {title : "My Dashboard",flashMessage: Flash.getMessage(req)});
+        res.redirect('/dashboard/create-profile')
+    }
+    catch(e){
+        next(e)
+    }
+
+}
+
+exports.editProfileGetController = async (req,res,next) =>{
+    try{
+        let profile = await Profile.findOne({user : req.user._id});
+        if(profile){
+            return res.redirect('dashboard/edit-profile')
+        }
+        res.render('pages/dashboard/create-profile', {title: 'Create Your Profile', flashMessage: Flash.getMessage(req)})
+    }
+    catch(e){
+        next(e)
+    }
 }
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -3,4 +3,9 @@ const {isAuthenticated} = require('../middleware/authMiddleware')
 const {dashboardGetController} = require('../controllers/dashboardController')
 router.get('/',isAuthenticated,dashboardGetController)
 
+router.get('/create-profile')
+router.post('/create-profile')
+
+router.get('/edit-profile')
+router.post('/edit-profile')
 module.exports = router
