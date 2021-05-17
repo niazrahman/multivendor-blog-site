f95ac658d1c105853a18fcfedafabb852faf9b1a
niazrahman <b170403065@sw.jnu.ac.bd>
2021-05-18 01:46:27 +0600

    dislike api created

--- api/controllers/likeDislikeController.js
+++ api/controllers/likeDislikeController.js
@@ -26,6 +26,7 @@ exports.likesGetController = async (req,res,next) => {
                 {_id : postId},
                 {$pull : {'likes' : userId}}
             )
+            liked = false
         }else{
             await Post.findOneAndUpdate(
                 {_id : postId},
@@ -46,4 +47,53 @@ exports.likesGetController = async (req,res,next) => {
             error : 'Server Error Occured'
         })
     }
+}
+
+exports.getDislikeController = async (req,res,next) =>{
+    let {postId} = req.params
+    let userId = req.user._id
+    let disliked = null
+
+    if(!req.user){
+        return res.status(403).json({
+            error : 'You are not an Authenticated user'
+        })
+    }
+
+    try{
+        let post = await Post.findById(postId)
+        if(post.likes.includes(userId)){
+            await Post.findOneAndUpdate(
+                {_id : postId},
+                {$pull : {'likes' : userId}}
+            )
+        }
+
+        if(post.dislikes.includes(userId)){
+            await Post.findOneAndUpdate(
+                {_id : postId},
+                {$pull : {'dislikes' : userId}}
+            )
+            disliked = false
+        }else{
+            await Post.findOneAndUpdate(
+                {_id : postId},
+                {$push : {'dislikes' : userId}}
+            )
+            disliked = true
+        }
+        let updatedPost = await Post.findById(postId)
+        res.status(200).json({
+            disliked,
+            totalLikes : updatedPost.likes.length,
+            totalDislikes : updatedPost.dislikes.length
+        })
+    }catch(e){
+        console.log(e)
+        return res.status(500).json({
+            error : 'Server Error Occured'
+        })
+    }
+
+
 }
