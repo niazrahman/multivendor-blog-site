fe54d038706cd6fd5ce3eafcc57efc586c6ed1f8
niazrahman <b170403065@sw.jnu.ac.bd>
2021-05-18 01:24:01 +0600

    like api created

--- api/controllers/commentController.js
+++ api/controllers/commentController.js
@@ -1,42 +1,42 @@
 const Post = require('../../models/Post');
 const Comment = require('../../models/Comment');
 
-exports.createPostController = async (req,res,next) => {
-    let {postId} = req.params
-    let {body} = req.body
+exports.commentPostController = async (req,res,next) => {
+    // let {postId} = req.params
+    // let {body} = req.body
 
-    if(!req.user){
-        return res.status(403).json({
-            error : 'You are not an Authenticated user'
-        })
-    }
+    // if(!req.user){
+    //     return res.status(403).json({
+    //         error : 'You are not an Authenticated user'
+    //     })
+    // }
 
-    let comment = new Comment ({
-        post : postId,
-        user : req.user._id, 
-        body,
-        replies : []
-    })
+    // let comment = new Comment ({
+    //     post : postId,
+    //     user : req.user._id, 
+    //     body,
+    //     replies : []
+    // })
 
-    try{
-        let createdComment = await comment.save()
-        await Post.findOneAndUpdate(
-            {_id : postId },
-            {$push : {'comments' : createdComment._id}}
-        )
+    // try{
+    //     let createdComment = await comment.save()
+    //     await Post.findOneAndUpdate(
+    //         {_id : postId },
+    //         {$push : {'comments' : createdComment._id}}
+    //     )
     
-        let commentJSON = await Comment.findById(createdComment._id).populate({
-            path : 'user',
-            select : 'profilePics username'
-        })
+    //     let commentJSON = await Comment.findById(createdComment._id).populate({
+    //         path : 'user',
+    //         select : 'profilePics username'
+    //     })
 
-        return res.status(201).json(commentJSON)
-    }catch(e){
-        console.log(e)
-        return res.status(500).json({
-            error : 'Server Error Occured'
-        })
-    }
+    //     return res.status(201).json(commentJSON)
+    // }catch(e){
+    //     console.log(e)
+    //     return res.status(500).json({
+    //         error : 'Server Error Occured'
+    //     })
+    // }
 }
 
 
@@ -60,6 +60,10 @@ exports.replyCommentPostController = async (req,res,next) => {
             {_id : commentId},
             {$push : {'replies' : reply}}
         )
+        res.status(201).json({
+            ...reply,
+            profilePics : req.user.profilePics
+        })
     }catch(e){
         console.log(e)
         return res.status(500).json({
--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -8,7 +8,10 @@ const {
 
 router.post('/comments/:postId', isAuthenticated,commentPostController)
 
-router.post('/comments/replies/:commentId', isAuthenticated,replyCommentPostController)
+// router.post('/comments/replies/:commentId', isAuthenticated,replyCommentPostController)
+
+router.get('/likes/:postId', isAuthenticated, (req, res) => {})
+router.get('/dislikes/:postId', isAuthenticated, (req, res) => {})
 
 
 module.expports = router
